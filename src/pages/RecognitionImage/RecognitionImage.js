55aa9902d041305f03cba0452ddd49d54dfa943d
Lua Mateus <104106208+Luamateus2@users.noreply.github.com>
2025-01-14 16:02:14 -0300

    upload de imagens concluida

--- src/pages/RecognitionImage/RecognitionImage.js
+++ src/pages/RecognitionImage/RecognitionImage.js
@@ -1,11 +1,18 @@
-// src/views/RecognitionImage.js
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../components/Hearder/Header';
 import useRecognitionController from '../../controllers/RecognitionController';
 import './RecognitionImage.css';
 
 const RecognitionImage = () => {
   const { fileName, result, handleFileChange, handleSubmit } = useRecognitionController();
+  const [imagePreview, setImagePreview] = useState(null);
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setImagePreview(URL.createObjectURL(file)); // Cria a URL da imagem para pré-visualização
+    }
+  };
 
   return (
     <>
@@ -22,7 +29,10 @@ const RecognitionImage = () => {
             <input
               type="file"
               id="file-upload"
-              onChange={handleFileChange}
+              onChange={(e) => {
+                handleFileChange(e);
+                handleImageChange(e); // Atualiza a pré-visualização da imagem
+              }}
               className="upload-input"
             />
             <span className="upload-file-name">
@@ -43,11 +53,35 @@ const RecognitionImage = () => {
             ) : (
               result.map((item, index) => (
                 <div key={index}>
+                  {/* Exibe as imagens lado a lado */}
+                  <div className="images-container">
+                    {/* Imagem carregada pelo usuário */}
+                    {imagePreview && (
+                      <div className="image-preview-container">
+                        <p className='upload-text'>Upload</p>
+                        <img src={imagePreview} alt="Imagem carregada" className="uploaded-image" />
+                      </div>
+                    )}
+
+                    {/* Imagem do banco */}
+                    {item.image_url ? (
+                      <div className="image-from-db-container">
+                        <p className='upload-text'>Base de Dados</p>
+                        <img src={item.image_url} alt="Imagem do Banco" className="uploaded-image" />
+                      </div>
+                    ) : (
+                      <p>Imagem não encontrada no banco.</p>
+                    )}
+                  </div>
+
                   <p><strong>Nome:</strong> {item.name}</p>
-                  <p>CPF:{item.cpf} </p>
-                  <p>RG:{item.rg}</p>
-                  <p>Nome do Pai:{item.nome_pai}</p>
-                  <p>Nome da mãe: {item.nome_mae}</p>  
+                  <p><strong>CPF:</strong>{item.cpf}</p>
+                  <p><strong>RG:</strong>{item.rg}</p>
+                  <p><strong>Nome do Pai: </strong>{item.nome_pai}</p>
+                  <p><strong>Nome da mãe:</strong>{item.nome_mae}</p>
+
+                  {/* Debugging: Verifique a URL da imagem */}
+                  {console.log("Imagem URL:", item.image_url)}
                 </div>
               ))
             )}
